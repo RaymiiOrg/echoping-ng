bda618772acadafa1ffdc14ed571720d6549f00e
Remy van Elst <RaymiiOrg@users.noreply.github.com>
2023-11-12 17:00:36 +0100

    Update and rename compilation.h.in to src/compilation.h

---
+++ src/compilation.h
@@ -0,0 +1,5 @@
+#define COMPILATION_OPTIONS unused
+
+// "@PACKAGE_NAME@ @PACKAGE_VERSION@ compiled with @CC@ on @hostname@ (@host@)\n at @compil_date@ with options:\nCFLAGS= @CFLAGS@\nLDFLAGS= @LDFLAGS@\n@compil_options@\n\nPlugins are searched in @plugins_dir@."
+
+/* $Id$ */
